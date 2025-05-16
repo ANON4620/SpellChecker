20ba8539437aabd934356905025ce784b74f8c5d
ANON4620 <swarnadeep.click@gmail.com>
2025-05-16 16:24:56 +0530

    Added files.

---
+++ readme.txt
@@ -0,0 +1,3 @@
+hello world. My name is Swarnadeep Sarkar.
+I am happy to tell you that I have Successfully written a program
+that checks spellings and outputs the words that are misspelled.
---
+++ spellchecker.py
@@ -0,0 +1,23 @@
+misspelled_words = []
+dict_file = "dictionary.txt"
+punctuation = '.,!"'
+
+# src_file = input("Enter source file: ")
+src_file = "readme.txt"
+with open(src_file, "r") as src:
+    data = src.read()
+
+    # filter words
+    for punc in punctuation:
+        data = data.replace(punc, " ")
+    print(data)
+    words = set(data.split())
+    print(words)
+    with open(dict_file, "r") as dict:
+        dict_words = set(dict.read().split())
+        for word in words:
+            if word.lower() not in dict_words:
+                misspelled_words.append(word)
+misspelled_words.sort()
+for word in misspelled_words:
+    print(word)
